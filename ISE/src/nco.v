3b95780b201b77f57e9dda871f12e2db7423805b
mazaczysko <miloszrygula@interia.pl>
2022-06-19 11:48:26 +0200

    First tunes played! Add vsynth_test

--- ISE/src/nco.v
+++ ISE/src/nco.v
@@ -1,26 +1,32 @@
 module nco (
 	input CLK,
-	//input CE,
+	input CE,
 	input [6:0] NOTE_NUM,
-	//input [6:0] NOTE_VEL,
-	//input [6:0] PROGRAM,
+	input [6:0] NOTE_VEL,
+	input [6:0] PROGRAM,
 	output[7:0] SAMPLE_OUT
 );
 
-parameter CE = 1;
+//parameter CE = 1;
 
 wire trig_sample;
 wire [7:0] sample_out;
 wire [7:0] sample;
 
-assign SAMPLE_OUT = sample_out;
+wire [15:0] sample_vel;
+
+assign sample_vel = sample_out * NOTE_VEL;
+
+assign SAMPLE_OUT = sample_vel[15:8];
+
+//assign SAMPLE_OUT = sample_out;
 
 wire [15:0] step;           //input of phase adder
 wire [15:0] phase_add_out;  //output of phase adder
 wire [15:0] phase_reg_out;  //input of phase adder
 
-//assign phase_add_out = phase_reg_out + step;
-assign phase_add_out = phase_reg_out + {16'h385};
+assign phase_add_out = phase_reg_out + step;
+//assign phase_add_out = phase_reg_out + {16'h385};
 
 
 //32kHz sample rate @ 100MHz CLK
@@ -58,9 +64,11 @@ phase2sample sampler(
     .CLK(CLK),
     .CE(trig_sample),
     .PHASE(phase_reg_out[15:9]),
-	.PROGRAM(NOTE_NUM),
+	.PROGRAM(PROGRAM),
     .SAMPLE_OUT(sample_out)
 );
+
+
 
 //Skip volume_rom (ISE error)
 
