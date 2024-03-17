f5fa28b607a2f3e34cdfdeb8a5151c88472d97e3
mazaczysko <miloszrygula@interia.pl>
2024-03-18 00:10:48 +0100

    Remove wavetable_ram clear sequence before load

--- sim/wavetable_loader_tb.v
+++ sim/wavetable_loader_tb.v
@@ -7,12 +7,17 @@ parameter RAM_SIZE = 61;
 reg clk;
 
 reg wtb_load;
+reg [4:0] wtb_num;
 
 wire [3:0] wtb_ram_we;      
 wire [5:0] wtb_ram_addr_w;  
 wire [7:0] wtb_ram_wfm_l_w;
 wire [7:0] wtb_ram_wfm_r_w;
 wire [7:0] wtb_ram_factor_w; 
+
+wire [4:0] done_wtb_num;
+wire       done;
+wire       idle;
 
 always #5 clk = ~clk;
 
@@ -24,18 +29,32 @@ wtb_load <= 1'b0;
 repeat (4) @(posedge clk);
 
 @(posedge clk)
+    wtb_num  <= 5'd0;
     wtb_load <= 1'b1;
 
 @(posedge clk)
     wtb_load <= 1'b0;
 
+@(posedge done)
+
+@(posedge clk)
+begin
+    wtb_load <= 1'b1;
+    wtb_num  <= 5'd1;
+end
+
+@(posedge clk)
+    wtb_load <= 1'b0;
+
+@(posedge done)
+    $finish;
 
 end
 
 wavetable_loader dut (
     .clk              ( clk  ),             
     
-    .wtb_num          ( 5'h00       ),
+    .wtb_num          ( wtb_num     ),
     .wtb_load         ( wtb_load    ),    
     .voice_num        ( 4'h0        ),
     
@@ -43,7 +62,11 @@ wavetable_loader dut (
     .wtb_ram_addr_w   ( wtb_ram_addr_w   ),
     .wtb_ram_wfm_l_w  ( wtb_ram_wfm_l_w  ),
     .wtb_ram_wfm_r_w  ( wtb_ram_wfm_r_w  ),
-    .wtb_ram_factor_w ( wtb_ram_factor_w )
+    .wtb_ram_factor_w ( wtb_ram_factor_w ),
+
+    .done_wtb_num   ( done_wtb_num       ),
+    .done           ( done               ),     
+    .idle           ( idle               )
 );
 
 
--- src/wavetables/wavetable_loader.v
+++ src/wavetables/wavetable_loader.v
@@ -24,7 +24,7 @@ localparam WTB_RAM_SIZE_W = 6;
 
 //FSM States
 parameter IDLE  = 3'd0;
-parameter CLEAR = 3'd1;
+parameter LOAD_OFFSET = 3'd1;
 parameter READ_WTB_DATA_L = 3'd2;
 parameter LOAD_PURE_L = 3'd3; 
 parameter READ_WTB_DATA_R = 3'd4;
@@ -35,9 +35,8 @@ parameter DONE = 3'd7;
 reg [2:0] fsm_state = IDLE;
 
 wire fsm_idle;
-wire fsm_clear;
 wire fsm_idle_and_wtb_load;
-wire fsm_clear_and_last_addr;
+wire fsm_load_offset;
 wire fsm_load_pure_l;
 wire fsm_load_pure_r;
 wire fsm_fill_factors;
@@ -78,9 +77,8 @@ wire [15:0] factor_16bit;
 wire  [7:0] factor;
 
 assign fsm_idle = fsm_state == IDLE;
-assign fsm_clear = fsm_state == CLEAR;
 assign fsm_idle_and_wtb_load = fsm_state == IDLE & wtb_load;
-assign fsm_clear_and_last_addr = fsm_state == CLEAR & wtb_ram_addr_cnt_co;
+assign fsm_load_offset = fsm_state == LOAD_OFFSET;
 assign fsm_load_pure_l = fsm_state == LOAD_PURE_L;
 assign fsm_load_pure_r = fsm_state == LOAD_PURE_R;
 assign fsm_fill_factors = fsm_state == FILL_FACTORS;
@@ -94,7 +92,7 @@ assign wtb_data_addr_wfm = wtb_data_addr_cnt_out;
 assign wtb_data_addr_pos = wtb_data_addr_cnt_out + 1'b1;
 
 assign wtb_data_addr_cnt_ce = fsm_state == READ_WTB_DATA_L | fsm_state == LOAD_PURE_L | READ_WTB_DATA_R;
-assign wtb_ram_addr_cnt_ce = fsm_state == FILL_FACTORS | fsm_clear;
+assign wtb_ram_addr_cnt_ce = fsm_state == FILL_FACTORS;
 
 assign wtb_ram_addr_cnt_ld_data = fsm_idle_and_wtb_load ? {WTB_RAM_SIZE_W{1'd0}} : wtb_data_pos;
 assign wtb_ram_addr_cnt_load = fsm_idle_and_wtb_load | fsm_load_pure_l;
@@ -105,16 +103,15 @@ assign distance_normalized = 16'hffff / distance_btwn_pure_waves;
 assign factor_16bit = distance_normalized * distance_to_left_pure;
 assign factor = factor_16bit[15:8];
 
-assign wtb_ram_write_enable = fsm_state == CLEAR | fsm_state == FILL_FACTORS;
+assign wtb_ram_write_enable = fsm_state == FILL_FACTORS;
 
 //Output definitions
 assign wtb_ram_addr_w  = wtb_ram_addr_cnt_out;
-assign wtb_ram_wfm_l_w = fsm_clear ? 8'h00 : (wtb_ram_addr_w == pure_r_pos_reg_out ? pure_r_wfm_reg_out : pure_l_wfm_reg_out); 
-assign wtb_ram_wfm_r_w = fsm_clear ? 8'h00 : (wtb_ram_addr_w == pure_l_pos_reg_out ? pure_l_wfm_reg_out : pure_r_wfm_reg_out);
+assign wtb_ram_wfm_l_w = (wtb_ram_addr_w == pure_r_pos_reg_out ? pure_r_wfm_reg_out : pure_l_wfm_reg_out); 
+assign wtb_ram_wfm_r_w = (wtb_ram_addr_w == pure_l_pos_reg_out ? pure_l_wfm_reg_out : pure_r_wfm_reg_out);
 assign wtb_ram_factor_w = fsm_fill_factors ? (wtb_ram_wfm_l_w == wtb_ram_wfm_r_w ? 8'h00 : factor) : 8'h00;
 assign wtb_ram_we = voice_num_reg_out[1] ? (voice_num_reg_out[0] ? {wtb_ram_write_enable, 3'b000} : {1'b0, wtb_ram_write_enable, 2'b00}) 
                                          : (voice_num_reg_out[0] ? {2'b00, wtb_ram_write_enable, 1'b0} : {3'b000, wtb_ram_write_enable});  
-
 
 assign done_wtb_num = wtb_num_reg_out;
 assign done = fsm_done;
@@ -198,7 +195,7 @@ wtb_data_addr_cnt
 (
     .clk     ( clk                      ),
     .ce      ( wtb_data_addr_cnt_ce     ),
-    .load    ( fsm_clear_and_last_addr  ),
+    .load    ( fsm_load_offset          ),
     .ld_data ( wtb_offset               ),   
     .q       ( wtb_data_addr_cnt_out    ), 
     .co      ( wtb_data_addr_cnt_co     )
@@ -246,13 +243,10 @@ begin
             if (!wtb_load)
                 fsm_state <= IDLE;
             else
-                fsm_state <= CLEAR;
+                fsm_state <= LOAD_OFFSET;
 
-        CLEAR:
-            if (!wtb_ram_addr_cnt_co)
-                fsm_state <= CLEAR;
-            else
-                fsm_state <= READ_WTB_DATA_L;
+        LOAD_OFFSET:
+            fsm_state <= READ_WTB_DATA_L;
         
         READ_WTB_DATA_L:
             fsm_state <= LOAD_PURE_L;
